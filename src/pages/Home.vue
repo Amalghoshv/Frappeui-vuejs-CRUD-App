c255a7dab410d6694c9ddcf96d5852db0f5f6ae4
Amalghosh <amalghosh123v@gmail.com>
2023-07-12 23:26:09 +0530

    first commit

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,31 +1,24 @@
 <template>
   <div>
-  <Layout>
-    <Sidebars @tabSelected="handleTabSelected" />
-  </Layout>
-  <component :is="activeTab"></component>
+    <Layout>
+      <Sidebars @tabSelected="handleTabSelected" />
+    </Layout>
+    <component :is="activeTab"></component>
     <router-view></router-view>
   </div>
 </template>
 
-
 <script>
 import Layout from '../components/Layout.vue'
-import { onMounted,ref, onBeforeMount  } from 'vue'
-import { useRoute, useRouter } from 'vue-router' 
-
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { Tabs, Tab, Button, Badge, Dropdown } from 'flowbite-vue'
 import 'flowbite'
 import router from '../router'
-import  Sidebars  from '../components/Sidebars.vue'
+import Sidebars from '../components/Sidebars.vue'
 import Dashboard from '../components/Dashboard.vue'
 import Navbar from '../components/Navbar.vue'
-import axios from 'axios';
 import BarChart from '../components/Barchart.vue'
-
-
-
-
 
 export default {
   name: 'Home',
@@ -38,105 +31,41 @@ export default {
       showText: true,
       buttonPosition: 'auto',
       showDropdown: false,
-      
     }
   },
   setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const items = ref([])
-    // const fetchData = () => {
-    //   axios
-    //     .get('https://propfi-erp.enfono.com/api/resource/Unit/', {
-    //       headers: {
-    //         Authorization: 'token 2e83ad6ac981c0e:6d9c75d97aee1c7',
-    //       },
-    //     })
-    //     .then(response => {
-    //       items.value = response.data
-    //       console.log(response.data)
-    //     })
-    //     .catch(error => {
-    //       console.error('Error fetching data:', error)
-    //       console.log('Error response:', error.response)
-    //       console.log('Error message:', error.message)
-    //     })
-    // }
-
-    onBeforeMount(() => {
-      // fetchData();
-    });
-
+    const route = useRoute()
+    const router = useRouter()
     onMounted(() => {
-      console.log('Current route:', route.value);
-    });
-   
-  const handleTabSelected =   tabName => {
-      this.activeTab = tabName;
+      console.log('Current route:', route.value)
+    })
+
+    const handleTabSelected = (tabName) => {
+      this.activeTab = tabName
       // Use the router to navigate to the selected tab
-      router.push('/' + tabName);
-    };
-   
-    
+      router.push('/' + tabName)
+    }
+
     return {
       items,
-      // fetchData,
-      handleTabSelected
-    };
+      handleTabSelected,
+    }
   },
-  
 
   methods: {
-   
-   
-    // fetchData() {
-    //   axios.get('https://propfi-erp.enfono.com/api/resource/Unit/',{
-    //     headers:{
-    //       Authorization: 'token 2e83ad6ac981c0e:6d9c75d97aee1c7'
-    //     }
-    //   })
-    //     .then(response => {
-    //       this.items = response.data;
-    //       console.log(response.data)
-    //     })
-    //     .catch(error => {
-    //       console.error('Error fetching data:', error);
-    //       console.log('Error response:', error.response);
-    //   console.log('Error message:', error.message);
-    //     });
-    // },
-    
     handleTabSelected(tabName) {
-      this.activeTab = tabName;
+      this.activeTab = tabName
       // Use the router to navigate to the selected tab
-      this.$router.push('/' + tabName);
+      this.$router.push('/' + tabName)
     },
     handleTabSelected(tabName) {
-      this.activeTab = tabName; // Update the active tab based on the selection
+      this.activeTab = tabName // Update the active tab based on the selection
     },
-    // getData() {
-    //   fetch('https://propfi-erp.enfono.com/api/resource/Unit/', {
-    //     headers: {
-    //       Authorization: 'token 2e83ad6ac981c0e:6d9c75d97aee1c7',
-    //     },
-    //   })
-    //     .then((r) => r.json())
-    //     .then((r) => {
-    //       console.log(r)
-    //     })
-    // },
-  
     handleItemClick(route) {
       this.$emit('item-clicked', route)
     },
     handleSidebarItemClick(route) {
       this.$router.push(route)
-    },
-    
-  },
-  resources: {
-    ping: {
-      url: 'ping',
     },
   },
 
@@ -147,12 +76,10 @@ export default {
     Tab,
     Badge,
     BarChart,
-    
     Sidebars,
     Dashboard,
     Navbar,
-
-    Layout
+    Layout,
   },
-  }
-</script  >
+}
+</script>
