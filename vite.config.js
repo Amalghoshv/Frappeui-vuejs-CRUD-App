c49b1e2d0bcc1c32fd465ad7ef1dd5fc34e0a2fc
Amalghosh <amalghosh123v@gmail.com>
2023-07-12 16:43:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
-import { webserver_port } from '../../../sites/common_site_config.json'
+import { getProxyOptions } from './vite-dev-server'
+import { webserver_port } from './common_site_config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
